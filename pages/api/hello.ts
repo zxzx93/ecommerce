a491c2a989b1bb0fea963af075ffb1f9557e41d3
yeonsu <thddustn26@hanmail.net>
2023-02-27 21:31:34 +0900

    feat: 디비 connect, disconnect

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+import db from '../../utils/db';
 
 type Data = {
-  name: string
-}
+  name: string;
+};
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  db.connectDb();
+  db.disconnectDb();
+  res.status(200).json({ name: 'John Doe' });
 }
---
+++ utils/db.js
@@ -0,0 +1,38 @@
+import mongoose from 'mongoose';
+
+const connection = {};
+
+async function connectDb() {
+  if (connection.isConnected) {
+    console.log('데이터베이스에 이미 연결됨');
+    return;
+  }
+  if (mongoose.connections.length > 0) {
+    connection.isConnected = mongoose.connections[0].readyState;
+    if (connection.isConnected === 1) {
+      console.log('데이터베이스에 대한 이전 연결을 사용합니다.');
+      return;
+    }
+    await mongoose.disconnect();
+  }
+  const db = await mongoose.connect(`${process.env.MONGODB_URL}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  console.log('데이터베이스에 새로 연결.');
+  connection.isConnected = db.connections[0].readyState;
+}
+
+async function disconnectDb() {
+  if (connection.isConnected) {
+    if (process.env.NODE_END === 'production') {
+      await mongoose.disconnect();
+      connection.isConnected = false;
+    } else {
+      console.log('데이터베이스에서 연결이 끊어지지 않습니다.');
+    }
+  }
+}
+
+const db = { connectDb, disconnectDb };
+export default db;
